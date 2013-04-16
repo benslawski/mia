84ae576268090c9260eedb3c372a81fa76610ab4
Mia <mia.oviatt@gmail.com>
2013-04-16 16:19:04 -0700

    don't remember what i changed

--- lab1/Lab1.java
+++ lab1/Lab1.java
@@ -1,5 +1,5 @@
-import java.io*;
-import Point3d;
+import java.io.*;
+import Point3d.java;
 
 /**
  * omething
@@ -12,7 +12,9 @@ public class Lab1 {
      *
      * @return return the inputted double, or 0 on error.
      */
-    public static double getDouble() {
+    public static double getDouble(String message) {
+
+        System.out.printIn(message);
 
         // There's potential for the input operation to "fail"; hard with a
         // keyboard, though!
@@ -46,18 +48,15 @@ public class Lab1 {
 
     /** Takes three ordered triples from the user and generates three Point3d objects. */
     public static void main(String [] args) {
-        Point3d point0 = new Point3d(getDouble(), getDouble(), getDouble());
-        Point3d point1 = new Point3d(getDouble(), getDouble(), getDouble());
-        Point3d point2 = new Point3d(getDouble(), getDouble(), getDouble());
+        Point3d point0 = new Point3d(getDouble(Point 0 x coord), getDouble(Point 0 y coord), getDouble(Point 0 z coord));
+        Point3d point1 = new Point3d(getDouble(Point 1 x coord), getDouble(Point 1 y coord), getDouble(Point 1 z coord));
+        Point3d point2 = new Point3d(getDouble(Point 2 x coord), getDouble(Point 2 y coord), getDouble(Point 2 z coord));
+        if (point0.equals(point1) || point1.equals(point2) || point0.equals(point2)) {
+            System.out.printI("Two or more points are the same. Please enter three different points.");
+            System.exit(0);
+    }
         double area = computeArea(point0, point1, point2);
         System.out.printf("area: %.4f", area);
-    }
-
-    /** Checks to see if any of the three points are the same. */
-    if point0.equals(point1) || point1.equals(point2) || point0.equals(point2) { 
-        System.out.printI("Two or more points are the same.  
-        Please enter three different points.");
-        System.exit(0);
     }
 
     /** Takes three Point3ds and computes the area within the triangle bounded by them. */
