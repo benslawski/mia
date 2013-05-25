d659e7fc5f4f035bd8c75fc761bc1ddd84f988cc
Mia <mia.oviatt@gmail.com>
2013-05-25 15:22:32 -0700

    lab6

--- lab6/Crawler.java
+++ lab6/Crawler.java
@@ -24,11 +24,9 @@ public class Crawler {
         }
         LinkedList<URLDepthPair> pendingURLs = newLinkedList<URLDepthPair>();
         LinkedList<URLDepthPair> processedURLs = newLinkedList<URLDepthPair>();
-        URLDepthPair currentDepthPair = new URLDepthPair;
-        currentDepthPair = (args[0], 0);
+        URLDepthPair currentDepthPair = new URLDepthPair(args[0], 0);
         pendingURLs.add(currentDepthPair);
         List<String> seenURLs = new List<String>;
-        seenURLs.add(args[0]);
         while (pendingURLs.size != 0) {
             depthPair = pendingURLs.pop();
             seenURLs.add(depthPair.getURL());
@@ -42,8 +40,7 @@ public class Crawler {
                 }
                 else {
                     seenURLs.add(newURL);
-                    URLDepthPair newDepthPair = new URLDepthPair;
-                    newDepthPair = (newURL, myDepth + 1);
+                    URLDepthPair newDepthPair = new URLDepthPair(newURL, myDepth + 1);
                     pendingURLs.add(newDepthPair);
                 }
             }
@@ -71,70 +68,3 @@ public class URLDepthPair {
     }
     // URL parsing and manipulation
 }
-public class Socket {
-    
-    public Socket(String host, int port) {
-        //Creates a new socket from a string representing the host and a port
-        // number, and makes the connection.
-    }
-    
-    void setSoTimeout(int timeout) {
-        // Sets the timeout of the Socket in milliseconds.  Call this after
-        // creating the socket so it knows how long to wait for data transfer
-        // from the other side.
-        
-    }
-    
-    InputStream getInputStream() {
-        //returns an inputstream associated with the socket.  this allows
-        // the socket to receive data from the other side of the connection.
-        
-    }
-    
-    OutputStream getOutputStream() {
-        // Returns an outputstream associated with the socket.  this allows the
-        // socket to send data to the other side of the connection
-    }
-    
-    getSites() {
-        
-    }
-    void close() {
-        //closes the socket
-    }
-}
-
-GET /people.html HTTP/1.1
-Host: www.cs.caltech.edu
-Connection: close
-
-Socket sock = new Socket(webHost, webPort);
-sock.setSoTimeout(3000); //Time-out after 3 seconds
-
-OutputStream os = sock.getOutputStream();
-
-// true tells PrintWrtier to flush after every output
-PrintWriter writer = new PrintWriter(os,true);
-
-writer.println("GET " + docPath + " HTTP/1.1");
-writer.println("Host: " + webHost);
-writer.println("Connection: close");
-writer.println();
-
-//Request is sent! Server will start responding now.
-
-//Response receiving code
-InputStream is = sock.getInputStream();
-InputStreamReader isr = new InpurStreamReader(is);
-BufferedReader br = new BufferedReader(isr);
-
-while (true) {
-    String line = br.readLine();
-    if (line == null)
-        break; // done reading document!
-    // Do something with this line of text.
-    System.out.println(line);
-
-}
-// method getAll URLs should take a URL and return a list of strings
-
