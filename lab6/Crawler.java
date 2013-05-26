7aa9ca10cc5e2b7709afcc41ebd6db9971125937
Mia <mia.oviatt@gmail.com>
2013-05-26 13:09:08 -0700

    lab6

--- lab6/Crawler.java
+++ lab6/Crawler.java
@@ -35,7 +35,7 @@ public class Crawler {
         LinkedList<URLDepthPair> processedURLs = new LinkedList<URLDepthPair>();
         URLDepthPair currentDepthPair = new URLDepthPair(args[0], 0);
         pendingURLs.add(currentDepthPair);
-        List<String> seenURLs = new List<String>();
+        ArrayList<String> seenURLs = new ArrayList<String>();
         seenURLs.add(currentDepthPair.getURL());
         
         while (pendingURLs.size != 0) {
@@ -112,44 +112,45 @@ public class Crawler {
         }
         return URLs;
     }
-}
-
-public class URLDepthPair {
     
-    public static final String URL_PREFIX = "http://";
-
-    private int currentDepth;
-    private String currentURL;
-    
-    public void URLDepthPair(String URL, int depth) {
-        currentDepth = depth;
-        currentURL = URL;
-    }
-    public String getURL() {
-        return currentURL;
-    }
-    public int getDepth() {
-        return currentDepth;
-    }
-    public String toString() {
-        return currentDepth.toString() + currentURL;
-    }
-    public String getDocPath() {
-        index = 0;
-        index = currentURL.indexOf(Crawler.END_URL);
-        String docPath = substring(index);
-        return docPath;
-    }
-    public String getwebHost() {
-        index = 0;
-        index = currentURL.indexOf(URL_PREFIX, index);
-        index += URL_PREFIX.length();
-        beginIndex = index;
-        index = currentURL.indexOf(Crawler.END_URL);
-        endIndex = index - 1;
-        String webHost = substring(beginIndex, endIndex);
-        return webHost;
-    }
+    public class URLDepthPair {
         
-
+        public static final String URL_PREFIX = "http://";
+        
+        private int currentDepth;
+        private String currentURL;
+        
+        public void URLDepthPair(String URL, int depth) {
+            currentDepth = depth;
+            currentURL = URL;
+        }
+        public String getURL() {
+            return currentURL;
+        }
+        public int getDepth() {
+            return currentDepth;
+        }
+        public String toString() {
+            return currentDepth.toString() + currentURL;
+        }
+        public String getDocPath() {
+            index = 0;
+            index = currentURL.indexOf(Crawler.END_URL);
+            String docPath = substring(index);
+            return docPath;
+        }
+        public String getwebHost() {
+            index = 0;
+            index = currentURL.indexOf(URL_PREFIX, index);
+            index += URL_PREFIX.length();
+            beginIndex = index;
+            index = currentURL.indexOf(Crawler.END_URL);
+            endIndex = index - 1;
+            String webHost = substring(beginIndex, endIndex);
+            return webHost;
+        }
+        
+        
+    }
 }
+
